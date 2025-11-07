8aa7b7c532cf38dc806dd6a4153b1b1427f92f9f
kamali <kamalikasubramanian@gmail.com>
2025-11-07 10:09:31 +0530

    Updates cors middleware in server side

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 5001;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","http://localhost:4173"],
     methods: ["GET", "POST", "PATCH", "DELETE", "PUT", "OPTIONS"],
     credentials: true,
   })
